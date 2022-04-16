e2aa1a7b705a8312b1afc32e8dae10950cd8d477
Furqan Iqbal <51705256+Furqan-Iqbal@users.noreply.github.com>
2022-04-16 10:13:44 +0200

    main test

---
+++ src/main/java/com/example/furqantest/FurqanTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.furqantest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FurqanTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FurqanTestApplication.class, args);
+    }
+
+}
